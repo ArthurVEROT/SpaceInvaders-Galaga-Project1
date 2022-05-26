c7330eaef19343359da8ae88d5e19ef46bd7d600
Arthur Vrt <46626919+ArthurVEROT@users.noreply.github.com>
2022-05-26 17:50:40 +0200

    styling

--- js/index.js
+++ js/index.js
@@ -4,21 +4,19 @@ const gamePage = document.querySelector(".gamepage");
 const playButton = document.getElementById("playButton");
 const startButton = document.getElementById("start-button");
 const pauseButton = document.getElementById("pause-button");
-// const homepageButton = document.querySelector("#homepage-button");
 
 const winMessage = document.querySelector("#win-message");
 const loseMessage = document.querySelector("#lose-message");
 
-let myNewGame = null;
+const myNewGame = null
 
 playButton.addEventListener("click", () => {
+  if (myNewGame) {return}
   homePage.classList.add("transform-left");
   gamePage.classList.add("transform-fade");
 
   gamePage.style.display = "flex";
-  gamePage.style.visibility = 'visible';
-  
-  
+  gamePage.style.visibility = "visible";
 
   myNewGame = new Game();
 });
